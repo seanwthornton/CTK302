3421004f7ae3b3f325f1444a84ec684880d64285
seanwthornton <swthorn@ilstu.edu>
2021-01-19 10:08:12 -0600

    Snowfella

---
+++ Unit1/snowman/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(200, 250, 255)
+  fill(255);
+    ellipse(width/2, height/2 + 75, 150, 150);
+    ellipse(width/2, height/2, 100, 100);
+    ellipse(width/2, height/2 - 60, 65, 65);
+
+
+}
