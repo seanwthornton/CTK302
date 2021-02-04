abca7bdecbe0fe37762720931ac066de0a7dc50b
seanwthornton <swthorn@ilstu.edu>
2021-02-04 08:40:59 -0600

    mp1 sound change3

--- Unit1/mp1/sketch.js
+++ Unit1/mp1/sketch.js
@@ -386,20 +386,20 @@ function draw() {
     noFill();
     arc(370, 270, 50, 20, 0, 40);
 
-    // if (mouseIsPressed) {
-    //   sound1.play();
-    // }else{
-    //   sound1.pause();
-    // }
+    if (mouseIsPressed) {
+      sound1.play();
+    }else{
+      sound1.pause();
+    }
 }
 
-function mouseReleased() {
-if (sound1.isPlaying()) {
-  sound1.pause();
-}else{
-  sound1.play();
-}
-}
+// function mouseReleased() {
+// if (sound1.isPlaying()) {
+//   sound1.pause();
+// }else{
+//   sound1.play();
+// }
+// }
 
 
 function mouseReleased() {
