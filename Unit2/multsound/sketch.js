0327e26902a9dbb0ec04e7189aadc878b3d163e8
seanwthornton <swthorn@ilstu.edu>
2021-02-24 08:27:46 -0600

    Unit 2 Multsound

--- Unit2/multsound/sketch.js
+++ Unit2/multsound/sketch.js
@@ -1,65 +1,72 @@
 let state = 0;
-let timer = 0;
+let song1, song2, song3;
+
+function preload() {
+  song1 = loadSound("assets/bensjazzfr.mp3");
+  song2 = loadSound("assets/benscute.mp3");
+  song3 = loadSound("assets/bensukulele.mp3");
+
+  song1.loop();
+  song1.pause();
+  song2.loop();
+  song2.pause();
+  song3.loop();
+  song3.pause();
+
+}
 
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(500, 500);
 
 }
 
 function draw() {
   background(100);
-
   switch (state) {
 
     case 0:
       text("1", 100, 100);
       song1.play();
-      state = 1
+      state = 1;
       break;
 
-      case 1:
-        text("listen to song 1", 100, 100);
-        break;
+    case 1:
+      text("listen to song 1", 100, 100);
+      break;
 
-        case 2:
-          song2.play();
-          state = 3
-          break;
+    case 2:
+      song2.play();
+      state = 3;
+      break;
 
-          case 3:
-            text("listen to song 2", 100, 100);
-            break;
+    case 3:
+      text("listen to song 2", 100, 100);
+      break;
 
-            case 4:
-              text("3", 100, 100);
-              song3.play();
-              state = 3
-              break;
+    case 4:
+      song3.play();
+      state = 5;
+      break;
 
-              case 4:
-                text("3", 100, 100);
-                song3.play();
-                state = 3
-                break;
+    case 3:
+      text("listen to song 3", 100, 100);
+      break;
+    }
   }
 
-  timer = timer + 1;
-  if(timer > 100) {
-    timer = 0;
+  function mouseReleased() {
+    song1.pause();
+    song2.pause();
+    song3.pause();
+
     state++;
-    if (state > 2) state = 0;
+    if (state > 5) state = 0;
+
+
   }
 
-}
 
-function mouseReleased() {
-  state++;
-  if (state > 2) state = 0;
-}
-
-
-function touchStarted() {
-  getAudioContext().resume();
-}//three magic lines of audio code!
-
-//three mp3 files!
+  // add these to programs that use sound, at the bottom
+  function touchStarted() {
+    getAudioContext().resume();
+  }
