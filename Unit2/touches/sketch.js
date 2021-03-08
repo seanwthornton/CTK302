a13101c2b593f7ae675ad362dfb9c0ff24eab411
seanwthornton <swthorn@ilstu.edu>
2021-03-07 22:21:10 -0600

    U2 30721 D

--- Unit2/touches/sketch.js
+++ Unit2/touches/sketch.js
@@ -14,14 +14,14 @@ function setup() {
 function draw() {
   clear();
   numberOfTouches = touches.length;
-  fill('white');
+  // fill('white');
   textSize(18);
   text(numberOfTouches + ' touches', width / 2, height / 2 - 50);
 
   switch (numberOfTouches) {
     case 0:
       background(100, 150, 170);
-      fill('white');
+      // fill('white');
       textSize(32);
       textFont(f1);
       text("Hey, touch the screen\nand see me cartooned!", width / 2, height / 2);
@@ -29,7 +29,7 @@ function draw() {
 
     case 1:
       STnorm = loadImage("assets/STnorm.jpg", width / 2, height / 2);
-      fill('white');
+      // fill('white');
       textSize(32);
       textFont(f1);
       text("Mild-mannered Sean", width / 2, height / 2 + 100);
@@ -38,7 +38,7 @@ function draw() {
 
     case 2:
       STspace = loadImage("assets/STspace.jpg", width / 2, height / 2);
-      fill('white');
+      // fill('white');
       textSize(32);
       textFont(f1);
       text("Space cadet Sean", width / 2, height / 2 + 100);
@@ -47,7 +47,7 @@ function draw() {
 
     case 3:
       STsupe = loadImage("assets/STsupe.jpg", width / 2, height / 2);
-      fill('white');
+      // fill('white');
       textSize(32);
       textFont(f1);
       text("Super Sean!", width / 2, height / 2 + 100);
