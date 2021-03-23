624db4e6dcf60bcaf71cb54262805e491c78c19b
seanwthornton <swthorn@ilstu.edu>
2021-03-23 09:24:19 -0500

    U3 mp3 A

--- Unit3/mp3/sketch.js
+++ Unit3/mp3/sketch.js
@@ -2,13 +2,23 @@ let cars = [];
 let frogPos;
 let state = 0;
 let timer = 0;
-let maxCars = 5 ;
-let maxTimer = 10 ;
+let maxCars = 5;
+let maxTimer = 10;
+let BallHog1 ;
+let BallHog2 ;
+let BallHog3 ;
+let BallHog4 ;
 
 function setup() {
   //all load commands go here ... images, fonts
   // createCanvas(windowWidth, windowHeight);
-  createCanvas(500, 500);
+  createCanvas(800, 800);
+  BallHog1 = loadImage("assets/BallHog1.jpg") ;
+  BallHog2 = loadImage("assets/BallHog2.jpg") ;
+  BallHog3 = loadImage("assets/BallHog3.jpg") ;
+  BallHog4 = loadImage("assets/BallHog4.jpg") ;
+
+  imageMode(CENTER);
 
   // Spawn an object
 
@@ -16,16 +26,17 @@ function setup() {
     cars.push(new Car());
   }
 
-  frogPos = createVector(width / 2, height / 2);
+  frogPos = createVector(width / 2, height - 100);
 
 }
 
 function draw() {
   switch (state) {
     case 0:
-      background('black');
-      fill('white');
-      text("Welcome! Click to play!", 100, 100); //make an image
+      // background('black');
+      image(BallHog1, width / 2, height / 2);
+      // fill('white');
+      // text("Wanna be the\nBALL HOG? Click my snout to play!", width/2, height/2); //make an image
       break;
     case 1:
       game();
@@ -37,15 +48,17 @@ function draw() {
       break;
 
     case 2: //win
-      background('green');
-      fill('white');
-      text("WIN! Click to play again.", 100, 100); //make an image
+      // background('green');
+      image(BallHog2, width / 2, height / 2);
+      // fill('white');
+      // text("What a BALL HOG! Click to play again.", 100, 100); //make an image
       break;
 
     case 3: //lose
-      background('red');
-      fill('white');
-      text("LOSER! Click to play again.", 100, 100); //make an image
+      // background('red');
+      image(BallHog3, width / 2, height / 2);
+      // fill('white');
+      // text("You're no ball hog! Click to play again.", 100, 100); //make an image
       break;
 
   }
@@ -58,27 +71,27 @@ function mouseReleased() {
       break;
 
     case 2: //they won, and they clicked to start again
-    resetGame() ;
+      resetGame();
       state = 0;
       break;
 
     case 3: //they lost, and they clicked to start again
-    resetGame() ;
+      resetGame();
       state = 0;
       break;
   }
 }
 
 function resetGame() {
-  timer = 0 ;
-  cars = [] ;
+  timer = 0;
+  cars = [];
   for (let i = 0; i < maxCars; i++) {
     cars.push(new Car());
   }
 }
 
 function game() {
-  background(100);
+  background(30, 130, 50);
   // do some actions on the object
   for (let i = 0; i < cars.length; i++) {
     cars[i].display();
@@ -95,9 +108,17 @@ function game() {
 
   checkForKeys();
 
-  //frog ... put it here
-  fill('green');
-  ellipse(frogPos.x, frogPos.y, 75, 75);
+  //frog ... put it here ... piggy
+  noStroke();
+  fill('pink');
+  ellipse(frogPos.x - 15, frogPos.y - 55, 10, 20); //ear
+    ellipse(frogPos.x + 15, frogPos.y - 55, 10, 20); //ear
+    rect(frogPos.x - 6, frogPos.y - 70, 12, 20)
+  ellipse(frogPos.x, frogPos.y - 45, 40, 40); //head
+  ellipse(frogPos.x, frogPos.y, 60, 80); //body
+  strokeWeight(5);
+  noFill();
+  arc(325, 350, 40, 120, 90, -HALF_PI);
 }
 
 
@@ -110,8 +131,10 @@ function checkForKeys() {
   if (keyIsDown(UP_ARROW)) frogPos.y -= 5;
   if (keyIsDown(DOWN_ARROW)) frogPos.y += 5;
 
-  if (frogPos.x > width) frogPos.x = 0 ;
-  if (frogPos.x < width) frogPos.x = 0 ;
+  // if (frogPos.x > width) frogPos.x = 0 ;
+  // if (frogPos.x < width) frogPos.x = 0 ;
+  // if (frogPos.y > width) frogPos.y = 0 ;
+  // if (frogPos.y < width) frogPos.y = 0 ;
 } //its own function, don't put in another function
 
 
@@ -137,7 +160,7 @@ class Car {
 
   display() { //decide what your objects are ... shape, images, text
     fill(this.col);
-    rect(this.pos.x, this.pos.y, this.width, 25); //change this for final!
+    ellipse(this.pos.x, this.pos.y, 25, 25); //change this for final!
     // textSize(this.width) ;
     // text("WOOHOO", this.pos.x, this.pos.y);
   }
@@ -151,4 +174,9 @@ class Car {
 
   }
 
+}
+
+// add these to programs that use sound, at the bottom
+function touchStarted() {
+  getAudioContext().resume();
 }
