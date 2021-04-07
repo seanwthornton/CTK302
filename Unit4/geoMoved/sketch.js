263eb4180386d18af6a024a72c087e54adc0b281
seanwthornton <swthorn@ilstu.edu>
2021-04-07 11:11:48 -0500

    U4 geo D

--- Unit4/geoMoved/sketch.js
+++ Unit4/geoMoved/sketch.js
@@ -1,7 +1,31 @@
-function setup() {
-  createCanvas(500, 500);
+var num ;
+var distance ;
+
+var locationData;
+function preload(){
+
+    locationData =  getCurrentPosition();
+}
+
+function setup(){
+	num = 0 ;
+    intervalCurrentPosition(positionPing, 5000) ;
+	createCanvas(displayWidth, displayHeight) ;
 }
 
 function draw() {
+
+
+}
+function positionPing(position){
+	textSize(24) ;
+	num++ ;
+	background(255) ;
+    text("lat: " + position.latitude, 10, 40);
+    text("long: " + position.longitude, 10, 90);
+	text("number of updates " + num, 10, 140) ;
+
+	distance = calcGeoDistance(locationData.latitude, locationData.longitude, position.latitude, position.longitude, 'mi') ;
+	text("you have moved " + distance, 10, 190) ;
 
 }
