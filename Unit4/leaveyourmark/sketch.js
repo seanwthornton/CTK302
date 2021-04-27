9e2809c7a0687e56a9605d69c7b01ce99be8d64b
seanwthornton <swthorn@ilstu.edu>
2021-04-26 21:38:42 -0500

    U4 mp4 plus

--- Unit4/leaveyourmark/sketch.js
+++ Unit4/leaveyourmark/sketch.js
@@ -1,20 +1,28 @@
+// take this survey: https://forms.gle/DydYY5s45FQtdmTF8
+
 var bubbles = [];
-var fire;
-var water;
-var earth;
-var air;
-var avengerslogo;
-var Mark ;
-var Dots ;
+var Mark;
+var Dots;
+let places = [];
+let lat = 0;
+let long = 0;
+let num = 0;
+let oldLocation = '' ;
+let myLocation = "Illinois State University";
+let allnames ;
+
+function preload() {
+
+}
 
 function setup() {
+  locationData = getCurrentPosition();
+  intervalCurrentPosition(positionPing, 5000);
 
+  pushPlaces();
 
   // Tabletop stuff, for getting google spreadsheet data in.
-  //let url = '1GtE3eoYVWBv9zMPoyettXzDCEv6qdNGKio_hgEh5duM'; // this is KEY of the URL from the sheet
-  // let url = '1-bKu2MweC4duGRTLNsv6kbvKors5x868IS8UeUTtnSk'; // this is KEY of the URL from the sheet
   let url = '1ZbfKqOaJuVfgBeo2z_qvbTsQ6-Fe7vv-Q0cmeKz9Yr4'; // this is KEY of the URL from the sheet
-  // https://docs.google.com/spreadsheets/d/1-bKu2MweC4duGRTLNsv6kbvKors5x868IS8UeUTtnSk/edit?usp=sharing
   let settings = {
     key: url, // The url of the published google sheet
     callback: gotData, // A callback for when the data comes in
@@ -31,19 +39,10 @@ function setup() {
   // Dots = loadImage("assets/Dots.png");
   imageMode(CENTER);
   f1 = loadFont("assets/VerlagBold.otf");
-  textSize(24);
   textAlign(CENTER);
   ellipseMode(CENTER);
   rectMode(CENTER);
   imageMode(CENTER);
-
-  // fire = loadImage('assets/firemagic.png');
-  // water = loadImage('assets/watermagic.png');
-  // earth = loadImage('assets/earthmagic.png');
-  // air = loadImage('assets/airmagic.png');
-  // avengerslogo = loadImage('assets/avengerslogo.png');
-
-
 
 }
 
@@ -52,72 +51,164 @@ function setup() {
 function gotData(data) {
 
   console.log(data); // Print the data in the console
-
-  // iterate through the array of data and create an object and push it on an array called bubbles
-  for (let i = 0; i < data.length; i++) {
-    bubbles.push(new Bubble(data[i].Name, data[i].Major, data[i].Quote, data[i].Hint)); // THESE Name and Shape need to match your column names in your spreadsheet!
-  }
-
+  allnames = data ;
+  makeBubbles() ;
 }
+
+function makeBubbles() {
+  // iterate through the array of data and create an object and push it on an array called bubbles
+  bubbles = [] ;
+  let yaxis = 0 ;
+  for (let i = 0; i < allnames.length; i++) {
+    if (allnames[i].Hint == myLocation) {
+      bubbles.push(new Bubble(allnames[i].Name, allnames[i].Major, allnames[i].Quote, allnames[i].Hint, yaxis * 120)); // THESE Name and Shape need to match your column names in your spreadsheet!
+      yaxis++ ; // only change the y axis of the next bubble if we found a bubble.
+    }
+  }
+}
+
 
 
 function draw() {
   background('#d41f2d');
   // image(Dots, width / 2, height / 2);
   image(Mark, width / 2, height / 2);
-  // image(avengerslogo, width/2, height/2, 900, 900);
 
-  // // iterate through the bubbles and display the objects!
+  textAlign(CENTER);
+  // textAlign(LEFT);
+  // text("lat: " + lat, 10, 340);
+  // text("long: " + long, 10, 360);
+  // text("number of updates: " + num, 10, 380);
+
+  push();
+  textSize(24);
+  // iterate through the bubbles and display the objects if their places match myPlace!
   for (let i = 0; i < bubbles.length; i++) {
     bubbles[i].display();
     bubbles[i].move();
+  }
+  pop();
+
+  fill('#d41f2d');
+  noStroke();
+  rect(width / 2, 0, windowWidth, 325);
+
+  fill('black');
+  textSize(48);
+  f1 = loadFont("assets/VerlagBold.otf");
+  text(myLocation, width / 2, 100);  // this prints out the location at the top
+
+}
+
+function positionPing(position) {
+  num++;
+  background(255);
+  lat = position.latitude.toFixed(8);
+  long = position.longitude.toFixed(8);
+
+  for (var i = 0; i < places.length; i++) {
+    if (places[i].fence.insideFence === true) {
+      myLocation = places[i].desc;
+      break; //should break out of the for loop?
+    }
+  }
+
+  if (myLocation != oldLocation) {
+    // redo bubble array
+    oldLocation = myLocation ;
+    makeBubbles() ;
+  }
+}
+
+// my Bubble class
+class Bubble {
+
+  constructor(myName, myMajor, myQuote, myHint, y) {
+    this.name = myName ; //.replace(/'/g, ''); // .replace(/'/g,'') strips the apostrophes out
+    this.major = myMajor ; //.replace(/'/g, '');
+    this.quote = myQuote ; //.replace(/'/g, '');
+    this.place = myHint; // .replace(/'/g, '');
+    this.pos = createVector(width / 2, y);
+    this.vel = createVector(0, -3);
+  }
+
+
+  display() {
+
+    if (myLocation == this.place) {
+      fill('white');
+      textFont(f1);
+      text(this.name, this.pos.x, this.pos.y - 25);
+      text(this.major, this.pos.x, this.pos.y);
+      text(this.quote, this.pos.x, this.pos.y + 25);
+      text(this.place, this.pos.x, this.pos.y + 50);
+
+    } else {
+      fill('grey');
+    }
+  }
+
+  move() {
+    this.pos.add(this.vel);
+    // if (this.pos.x > width) this.pos.x = 0;
+    // if (this.pos.x < 0) this.pos.x = width;
+    // if (this.pos.y > height) this.pos.y = 0;
+    if (this.pos.y < 0) this.pos.y = height + 600;
   }
 
 }
 
 
-// my Bubble class
-class Bubble {
 
-  constructor(myName, myMajor, myQuote, myHint) {
-    this.name = myName;
-    this.major = myMajor;
-    this.quote = myQuote;
-    this.hint = myHint;
-    this.pos = createVector(random(width), random(height));
-    this.vel = createVector(random(-5, 5), random(-5, 5));
+function pushPlaces() {
+  places.push(new Place(40.19580389554057, -88.41188678700051, "Lauren's House", .02)); // new Place object, for CVA room 17
+  places.push(new Place(40.512216613792866, -88.98539072302248, "Rafael's House", .02)); // new Place object, for CVA room 17
+  places.push(new Place(40.53556089911177, -88.9757035113809, "Sean's House", .02)); // new Place object, for CVA room 17
+  places.push(new Place(40.47859881213726, -88.96815846900026, "Rose's House", .02)); // new Place object, for CVA room 17
+  places.push(new Place(40.50622797365503, -88.99051350503431, "CVA 17", .02)); // new Place object, for CVA room 17
+  places.push(new Place(40.50715473783438, -88.99173550368103, "COB", .02)); // new Place object, for COB.... JUST SWITCHED TO NEW COORDINATES
+  places.push(new Place(40.510824736433904, -88.99134151266699, "ISU College Bridge", .02)); // new Place object, for ISU bridge over College Ave
+  places.push(new Place(40.50863221414712, -88.99077591254148, "Old Union", .02)); // new Place object, for ISU bridge over College Ave
+  places.push(new Place(40.50840289459472, -88.9909118880512, "Williams Hall", .02)); // new Place object, for ISU bridge over College Ave
+  places.push(new Place(40.50844449497366, -88.9911676488728, "Cent 4 Perf Arts", .02)); // new Place object, for ISU bridge over College Ave
+  places.push(new Place(40.50750741811689, -88.99029850463533, "CVA Circle", .02)); // new Place object, for ISU bridge over College Ave
+  places.push(new Place(40.5079598008354, -88.99148671475066, "McCormick Hall", .02)); // new Place object, for ISU bridge over College Ave
+  places.push(new Place(40.50864821960959, -88.99120123764614, "Fell Hall", .02)); // new Place object, for ISU bridge over College Ave
+  places.push(new Place(40.50896516728555, -88.99212919431163, "DeGarmo Hall Solar", .02)); // new Place object, for ISU bridge over College Ave
+  places.push(new Place(40.50917235949953, -88.99177097641105, "Cook Hall", .02)); // new Place object, for ISU bridge over College Ave
+  places.push(new Place(40.50947612369081, -88.99174125561485, "Edwards Hall", .02)); // new Place object, for ISU bridge over College Ave
+  places.push(new Place(40.50980921328644, -88.99149564020013, "Schroeder Hall", .02)); // new Place object, for ISU bridge over College Ave
+  places.push(new Place(40.51014656358694, -88.9912748009074, "Felmley Hall", .02)); // new Place object, for ISU bridge over College Ave
+  places.push(new Place(40.50942119788225, -88.99076697407767, "Moulton Hall", .02)); // new Place object, for ISU bridge over College Ave
+  places.push(new Place(40.50930542714758, -88.99068753755874, "Hovey Hall", .02)); // new Place object, for ISU bridge over College Ave
+  places.push(new Place(40.508730456808415, -88.98572041960726, "University Galleries", .02)); // new Place object, for ISU bridge over College Ave
+  places.push(new Place(40.511676506705506, -88.9938474159579, "Student Services Building", .02)); // new Place object, for ISU bridge over College Ave
+  places.push(new Place(40.51039740494815, -88.9996815241351, "Nelson Smith", .02)); // new Place object, for ISU bridge over College Ave
+  places.push(new Place(40.512969573012896, -88.99488587696477, "Hancock Stadium", .02)); // new Place object, for ISU bridge over College Ave
+  places.push(new Place(40.509699669963155, -88.99664232253424, "Turner Hall", .02)); // new Place object, for ISU bridge over College Ave
+  places.push(new Place(40.512242304908746, -88.99975734818341, "Tri Towers (Haynie)", .02)); // new Place object, for ISU bridge over College Ave
+  places.push(new Place(40.50946288329222, -88.98459824742137, "Uptown Circle", .02)); // new Place object, for ISU bridge over College Ave
+  places.push(new Place(40.47137301266825, -88.94350239220492, "Check Location", .02)); // new Place object, for ISU bridge over College Ave
+  places.push(new Place(40.511276, -88.9911698, "Milner", .02));
+  places.push(new Place(40.5087355, -88.9892591, "Stevenson Hall", .02));
+
+
+}
+
+class Place {
+  constructor(lat, long, desc, radius) {
+    this.lat = lat;
+    this.long = long;
+    this.fence = false;
+    this.desc = desc;
+    this.radius = radius;
+    this.fence = new geoFenceCircle(this.lat, this.long, this.radius); //sets geofence location to coordinates
   }
 
 
   display() {
-    // if (this.shape == "Square") {
-    //   rect(this.pos.x, this.pos.y, 50, 50);
-    // } else {
-    //   ellipse(this.pos.x, this.pos.y, 50, 50);
-    // }
-
-    // if(this.element == 'Fire') image(fire, this.pos.x, this.pos.y, 100, 100);
-    // if(this.element == 'Water') image(water, this.pos.x, this.pos.y, 100, 100);
-    // if(this.element == 'Earth') image(earth, this.pos.x, this.pos.y, 100, 100);
-    // if(this.element == 'Air') image(air, this.pos.x, this.pos.y, 100, 100);
-
-    //rect(this.pos.x, this.pos.y, 100, 100);
-    textFont(f1);
-    fill('white');
-    text(this.name, this.pos.x, this.pos.y - 25);
-    text(this.major, this.pos.x, this.pos.y);
-    text(this.quote, this.pos.x, this.pos.y + 25);
-    text(this.hint, this.pos.x, this.pos.y + 50);
-
-
+    //    image(reggieImg, 10, 10);
+    textSize(20);
+    text("You are at " + this.desc, 100, 400);
   }
-
-  move() {
-    this.pos.add(this.vel);
-    if (this.pos.x > width) this.pos.x = 0;
-    if (this.pos.x < 0) this.pos.x = width;
-    if (this.pos.y > height) this.pos.y = 0;
-    if (this.pos.y < 0) this.pos.y = height;
-  }
-
 }
