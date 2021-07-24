971140cc3527de205e507f70a8576477a6ce4218
seanwthornton <swthorn@ilstu.edu>
2021-07-24 15:41:28 -0500

    CTK490 terrain Rose

--- CTK490/perlin2DMapImages2/sketch.js
+++ CTK490/perlin2DMapImages2/sketch.js
@@ -9,7 +9,7 @@ let forest = "#325d17";
 let hills = "#4e7c31";
 let mountains = "#676767";
 
-function setup() {
+function preload(){
   terrain[0] = loadImage("assets/dirt.png");
   terrain[1] = loadImage("assets/forest.png");
   terrain[2] = loadImage("assets/grass.png");
@@ -18,21 +18,45 @@ function setup() {
   terrain[5] = loadImage("assets/sand.png");
   terrain[6] = loadImage("assets/swamp.png");
   terrain[7] = loadImage("assets/water.png");
+}
 
-  createCanvas(1920, 1080);
+function setup() {
+  // terrain[0] = loadImage("assets/dirt.png");
+  // terrain[1] = loadImage("assets/forest.png");
+  // terrain[2] = loadImage("assets/grass.png");
+  // terrain[3] = loadImage("assets/hills.png");
+  // terrain[4] = loadImage("assets/mountains.png");
+  // terrain[5] = loadImage("assets/sand.png");
+  // terrain[6] = loadImage("assets/swamp.png");
+  // terrain[7] = loadImage("assets/water.png");
+
+  // createCanvas(1920, 1080);
+  createCanvas(400, 400);
+
   noStroke();
-  colorMode (HSB);
-for (let i = 0; i < width/tileSize; i ++) {
-  for (let j = 0; j < height/tileSize; j++) {
-    // fill(pickTile(i, j));
-    image(terrain[getTile(i, j)], i * tileSize, j * tileSize, tileSize, tileSize);
+  // colorMode(HSB);
+  for (let i = 0; i < width / tileSize; i++) {
+    for (let j = 0; j < height / tileSize; j++) {
+      // fill(pickTile(i, j));
+      // image(terrain[0], i * tileSize, j * tileSize, tileSize, tileSize);
+      // image(terrain[1], 40, 40, 40, 40);
+      // fill(random(255));
+      // rect(i * tileSize, j * tileSize, tileSize, tileSize);
+      image(terrain[getTile(i, j)], i * tileSize, j * tileSize, tileSize, tileSize);
+      // image(terrain[0], i * tileSize, j * tileSize, tileSize, tileSize);
+      image(terrain[1], 40, 40, 40, 40);
+
+    }
 
   }
 }
-}
 
 function draw() {
-
+  // for (let i = 0; i < width / tileSize; i++) {
+  //   for (let j = 0; j < height / tileSize; j++) {
+  //     image(terrain[getTile(i, j)], i * tileSize, j * tileSize, tileSize, tileSize);
+  //   }
+  // }
 }
 
 // function keyPressed() {
@@ -49,17 +73,20 @@ function getTile(i, j) {
   if (h < 0.3) {
     c = 7;
   } else if (h < 0.4) {
-      c = 5;
-    }else if (h < 0.5) {
-      c = 2;
-    }else if (h < 0.6) {
-      c = 3;
-    } else if (h < 0.7) {
-      c = 1;
-    }else {
+    c = 5;
+  } else if (h < 0.5) {
+    c = 2;
+  } else if (h < 0.6) {
+    c = 3;
+  } else if (h < 0.7) {
+    c = 1;
+  } else {
     c = 4;
   }
-  return color(c);
+  // return color(c);
+  // print(c);
+  return c;
+
 }
 //
 // function drawMap() {
